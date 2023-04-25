64293883d9af779667c43a4a25cc9b993d16c722
emmakotey <emmakotey@gmail.com>
2023-04-25 15:16:29 +0000

    once

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,20 +1,16 @@
 #!/usr/bin/node
-
-// a script to count items form an api call
-
 const request = require('request');
-const url = process.argv[2];
-request(url, (err, res, body) => {
-  if (err) console.error(err);
-  const tasks = JSON.parse(body);
-  const dict = {};
-  for (let i = 0; i < tasks.length; i++) {
-    if (!dict[tasks[i].userId]) {
-      dict[tasks[i].userId] = 0;
-    }
-    if (tasks[i].completed === true) {
-      dict[tasks[i].userId] += 1;
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    let completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
   }
-  console.log(dict);
 });

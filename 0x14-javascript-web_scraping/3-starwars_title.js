5802c465d41d86e4bc50738c0a61c806e62c0abf
emmakotey <emmakotey@gmail.com>
2023-04-25 14:48:13 +0000

    once

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,4 +3,4 @@ const request = require('request');
 let url = 'http://swapi.co/api/films/' + process.argv[2];
 request(url, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
-});
+};

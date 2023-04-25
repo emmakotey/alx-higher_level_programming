60d551903cbbe098906feb118e8564b38292e223
emmakotey <emmakotey@gmail.com>
2023-04-25 15:06:31 +0000

    once

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,19 +1,7 @@
 #!/usr/bin/node
-// Import required modules
+
 const request = require('request');
-
-// Fetch the episode number from command line argument
-const episodeNumber = process.argv[2];
-
-// Make a GET request to the Star Wars API with the provided movie ID
-request(`https://swapi-api.alx-tools.com/api/films/${episodeNumber}`, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-  } else if (response.statusCode !== 200) {
-    console.error(`Error: ${response.statusCode} - ${response.statusMessage}`);
-  } else {
-    const movie = JSON.parse(body);
-    console.log(movie.title);
-  }
+let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  console.log(error || JSON.parse(body).title);
 });
-

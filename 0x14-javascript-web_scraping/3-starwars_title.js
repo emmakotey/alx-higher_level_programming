f2ea8b61c8de5401202039a692e1b001d14ac178
emmakotey <emmakotey@gmail.com>
2023-04-25 15:12:39 +0000

    once

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 
+// printing the status code of a GET request
+
 const request = require('request');
-let url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, (err, res, body) => {
+  if (err) console.error(err);
+  const data = JSON.parse(body);
+  console.log(data.title);
 });
